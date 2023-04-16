80e1bdcbae22206fec6438dab02e91e1ff796a31
Renrui Zhang <54577425+ZrrSkywalker@users.noreply.github.com>
2023-04-17 01:39:44 +0800

    Update extract_adapter_from_checkpoint.py

--- alpaca_finetuning_v1/extract_adapter_from_checkpoint.py
+++ alpaca_finetuning_v1/extract_adapter_from_checkpoint.py
@@ -1,6 +1,7 @@
 import torch
-import pdb
-pdb.set_trace()
+
+
+
 model = torch.load('./checkpoint/checkpoint-4.pth', map_location='cpu')
 new_model = dict()
 weight_list = ['layers.' + str(i) + '.attention.gate' for i in range(32)]
@@ -8,9 +9,7 @@ old_weight_list = ['layers.' + str(i) + '.attention.gate' for i in range(32)]
 weight_list = weight_list + ['adapter_query.weight']
 
 print(weight_list)
-
 print(model['model']['adapter_query.weight'].shape)
-
 
 for i in range(len(weight_list)):
         new_model[weight_list[i]] = model['model'][weight_list[i]]
