aa4fc6cec8537a4d9195c587d034e76d463659b6
Ziyu Guo <61613867+ZiyuGuo99@users.noreply.github.com>
2023-06-05 15:04:53 +0800

    Create demo_3d.py

---
+++ imagebind_LLM/demo_3d.py
@@ -0,0 +1,20 @@
+import ImageBind.data as data
+import llama
+
+
+llama_dir = "/path/to/LLaMA"
+
+model = llama.load("7B-beta", llama_dir, knn=True)
+model.eval()
+
+inputs = {}
+point = data.load_and_transform_point_cloud_data(["examples/airplane.pt"], device='cuda')
+inputs['Point'] = [point, 1]
+
+results = model.generate(
+    inputs,
+    [llama.format_prompt("Describe the 3D object in detail.")],
+    max_gen_len=256
+)
+result = results[0].strip()
+print(result)
