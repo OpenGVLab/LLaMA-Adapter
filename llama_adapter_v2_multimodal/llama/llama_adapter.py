aa155d1dc7b4a90d2520e9cc364cf7de6c01aa64
Enderfga <qq2639135175@gmail.com>
2023-07-08 22:06:51 +0800

    save the model

--- llama_adapter_v2_multimodal/llama/llama_adapter.py
+++ llama_adapter_v2_multimodal/llama/llama_adapter.py
@@ -273,6 +273,31 @@ class LLaMA_adapter(nn.Module):
 
         return decoded
 
+    def save(self, path):
+        visual_block_keys = ['norm1', 'attn.qkv', 'attn.proj', 'norm2', 'mlp.fc1', 'mlp.fc2']
+        base_keys_extra = ['clip_proj_norm', 'visual_proj_norm', 'visual_proj', 'clip_proj']
+        suffixes = ['weight', 'bias']
+
+        base_keys_visual = [f'{key}.{suffix}' for key in visual_block_keys for suffix in suffixes]
+        base_keys_llama = ['attention.gate', 'attention.wq.bias', 'attention.wo.bias', 'feed_forward.w1.bias', 'feed_forward.w2.bias', 'feed_forward.w3.bias', 'attention_norm.weight', 'ffn_norm.weight']
+        extra_keys = [f'{base_key}.{suffix}' for base_key in base_keys_extra for suffix in suffixes]
+        extra_keys.extend(['llama.norm.weight', 'visual_query.weight', 'adapter_query.weight']) 
+        
+        keys = []
+        for i in range(8):
+            for base_key in base_keys_visual:
+                keys.append(f'visual_blocks.{i}.{base_key}')
+        for i in range(32):
+            for base_key in base_keys_llama:
+                keys.append(f'llama.layers.{i}.{base_key}')
+        keys += extra_keys
+
+        full_model_state_dict = self.state_dict()
+        small_weights = {key: full_model_state_dict[key] for key in keys}
+        wrapped_small_weights = {'model': small_weights}
+
+        # Save the wrapped small weights
+        torch.save(wrapped_small_weights, path)
 
 _MODELS = {
     "BIAS-7B": "https://github.com/OpenGVLab/LLaMA-Adapter/releases/download/v.2.0.0/7fa55208379faf2dd862565284101b0e4a2a72114d6490a95e432cf9d9b6c813_BIAS-7B.pth",
@@ -294,7 +319,7 @@ def load(name, llama_dir, device="cuda" if torch.cuda.is_available() else "cpu",
         return RuntimeError(f"Model {name} not found; available models = {available_models()}"), None
 
     # BIAS-7B or https://xxx/sha256_BIAS-7B.pth -> 7B
-    llama_type = name.split('.')[0].split('-')[-1]
+    llama_type = name[-6:-4]#name.split('.')[0].split('-')[-1]
     llama_ckpt_dir = os.path.join(llama_dir, llama_type)
     llama_tokenzier_path = os.path.join(llama_dir, 'tokenizer.model')
 
